2359395893c43a0b671c48c07ef648f3440079e2
AlejandroTrejoCode <alejandrotrejocode@gmail.com>
2018-05-27 18:27:26 -0500

    DB Ready

---
+++ application/core/database.php
@@ -0,0 +1,9 @@
+<?php
+
+    const SERVER = 'localhost';
+    const DB = 'botanicqr';
+    const USER = "bqruser";
+    const PASSWORD = "botanicqr123";
+    const DBDRIVER = 'mysql:host='.SERVER.';dbname='.DB.';charset=UTF8';
+
+?>
--- application/core/dbConection.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$db = new PDO('mysql:host=localhost;dbname=botanicqr;charset=utf8mb4', 'bqruser', 'botanicqr123');
-// Conexion para pruebas $db = new PDO('mysql:host=localhost;dbname=carritoexample;charset=utf8mb4','root','');
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
-?>
