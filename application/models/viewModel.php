0d451bccb44207ab1f7d94fb23c4a1a7856557d8
AlejandroTrejoCode <alejandrotrejocode@gmail.com>
2018-05-27 18:11:38 -0500

    Model, controller of views added.

--- application/models/viewModel.php
+++ application/models/viewModel.php
@@ -3,7 +3,21 @@
     class viewModel {
 
         protected function getViewModel( $view ) {
-            
+
+            $validRoutes = ["inicio", "explorar", "noticias", "404"];
+
+            if (in_array($view, $validRoutes)) {
+
+                $currentView = $view . ".php";
+
+            } elseif ($view == "index") {
+                $currentView = "inicio.php";
+            } else {
+                $currentView = "404.php";
+            }
+
+            return $currentView;
+
         }
 
     }
---
+++ application/views/inicio.php
@@ -0,0 +1,5 @@
+<?php require_once 'application/views/templates/notices-cards.php'?>
+
+<?php require_once 'application/views/templates/call.php'?>
+
+<?php require_once 'application/views/templates/plants-cards.php'?>
