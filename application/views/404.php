a3fc77cba85d7377bd4fc8689d6fa48744b452a0
AlejandroTrejoCode <alejandrotrejocode@gmail.com>
2018-05-27 18:47:25 -0500

    404 Page Thanks: Olia Gozha

--- application/views/404.php
+++ application/views/404.php
@@ -1,1 +1,8 @@
-<h1>No existe papu</h1>
+<div class="error-page flex justify">
+    <div class="container">
+        <div class="column align justify">
+            <img class="responsive-img" src="<?php echo SERVER_URL ?>application/assets/img/404.jpg" alt="">
+            <h2>Lo sentimos, la página que solicitas no existe</h2>
+        </div>
+    </div>
+</div>
