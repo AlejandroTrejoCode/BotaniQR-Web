3a4a6a1871771004a7e2675dc9ac7ead11e1bdf5
AlejandroTrejoCode <alejandrotrejocode@gmail.com>
2018-07-20 20:36:18 -0500

    NO se que es

--- application/views/templates/plants-cards.php
+++ application/views/templates/plants-cards.php
@@ -99,6 +99,101 @@
 
                 </div>
             </div>
+
+            <div class="row">
+                <div class="cards-container row-responsive">
+                    <div class="plant-card">
+                        <div class="column">
+                            <div class="card-image responsive-img">
+                                <img src="<?php echo SERVER_URL?>application/assets/img/planta1.jpg" alt="" title="">
+                            </div>
+                            <div class="card-title justify">
+                                <h3>Nullam ullamcorper nisl quis ornare molestie</h3>
+                            </div>
+                            <div class="card-details row">
+                                <div class="card-status">
+                                    <div class="column justify">
+                                        <i class="fa fa-thermometer-half" aria-hidden="true"></i>
+                                    </div>
+                                </div>
+                                <div class="card-description">
+                                    <div class="column">
+                                        <p>
+                                            Suspendisse posuere, diam in bibendum lobortis, turpis ipsum aliquam risus, sit
+                                        </p>
+                                        <a href="#" class="read-more">
+                                            Conocer Más
+                                            <i class="fa fa-angle-right" aria-hidden="true"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="plant-card">
+                        <div class="column">
+                            <div class="card-image responsive-img">
+                                <img src="<?php echo SERVER_URL?>application/assets/img/planta2.jpg" alt="" title="">
+                            </div>
+                            <div class="card-title justify">
+                                <h3>Nullam ullamcorper nisl quis ornare molestie</h3>
+                            </div>
+                            <div class="card-details row">
+                                <div class="card-status">
+                                    <div class="column justify">
+                                        <i class="fa fa-thermometer-half" aria-hidden="true"></i>
+                                    </div>
+                                </div>
+                                <div class="card-description">
+                                    <div class="column">
+                                        <p>
+                                            Suspendisse posuere, diam in bibendum lobortis, turpis ipsum aliquam risus, sit
+                                        </p>
+                                        <a href="#" class="read-more">
+                                            Conocer Más
+                                            <i class="fa fa-angle-right" aria-hidden="true"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="plant-card">
+                        <div class="column">
+                            <div class="card-image responsive-img">
+                                <img src="<?php echo SERVER_URL?>application/assets/img/planta1.jpg" alt="" title="">
+                            </div>
+                            <div class="card-title justify">
+                                <h3>Nullam ullamcorper nisl quis ornare molestie</h3>
+                            </div>
+                            <div class="card-details row">
+                                <div class="card-status">
+                                    <div class="column justify">
+                                        <i class="fa fa-thermometer-half" aria-hidden="true"></i>
+                                    </div>
+                                </div>
+                                <div class="card-description">
+                                    <div class="column">
+                                        <p>
+                                            Suspendisse posuere, diam in bibendum lobortis, turpis ipsum aliquam risus, sit
+                                        </p>
+                                        <a href="#" class="read-more">
+                                            Conocer Más
+                                            <i class="fa fa-angle-right" aria-hidden="true"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
+
         </div>
     </div>
 </div>
