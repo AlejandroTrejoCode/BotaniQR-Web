76be67d7cdae7a3ffcfed61d65487dc4a7842204
AlejandroTrejoCode <alejandrotrejocode@gmail.com>
2018-07-28 23:52:31 -0500

    Header en Trejocode

--- application/views/template.php
+++ application/views/template.php
@@ -3,18 +3,16 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title> BotaniQR </title>
-    <meta name="author" content="Universidad Politécnica de Quitana Roo">
-    <meta name="description" content="BotaniQR permite a estudiantes, administrativos e invitados, la visualización de información relacionada con el Huerto de la universidad así como el monitoreo de riego.">
+    <title>UPQROO - BotaniQR </title>
+    <meta name="author" content="Universidad Politécnica de Quitana Roo | TrejoCode">
     <meta name="keywords" content="BotaniQR, UPQROO, Huerto, Plantas, Vivero, Riego">
-    <meta name="robots" content="noindex">
+    <meta name="description" content="BotaniQR permite a estudiantes, administrativos e invitados, la visualización de información relacionada con el Huerto de la universidad">
     <link rel="shortcut icon" type="image/x-icon" href="<?php echo SERVER_URL ?>favicon.ico">
+    <link rel="stylesheet" href="<?php echo SERVER_URL ?>application/assets/css/trejocode.css" media="screen">
     <link rel="stylesheet" href="<?php echo SERVER_URL ?>application/assets/css/style.css" media="screen">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet"> 
-    <link rel="stylesheet" href="<?php echo SERVER_URL; ?>application/assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta name="theme-color" content="#33c8a3">
 </head>
 <body>
 
