21bcb02740dc4355d6aaff2be6ac213c42c17136
AlejandroTrejoCode <alejandrotrejocode@gmail.com>
2018-08-02 17:30:07 -0500

    Planta Arreglada bras

--- application/views/planta.php
+++ application/views/planta.php
@@ -1,56 +1,70 @@
-<div class="white-space-25"></div>
-<div class="planta flex justify">
-    <div class="container">
-        <div class="column">
-            <div class="title">
-                <h2>Chile Piquín</h2>
-            </div>
-            <div class="cname">
-                <h3>Capsicum annuum var. glabriusculum</h3>
-            </div>
-            <div class="white-space-8"></div>
-            <div class="row-responsive">
-                <div class="plant-container">
-                    <div class="left">
-                        <div class="responsive-img" id="planta-img">
-                            <img src="<?php echo SERVER_URL?>application/assets/img/planta2.jpg" alt="Planta">
-                        </div>
+
+<div class="white-space-32"></div>
+<div class="planta flex">
+
+    <div class="column">
+        <div class="row justify-center">
+            <div class="container">
+                <div class="column">
+                    <div class="title">
+                        <h2>Chile Piquín</h2>
                     </div>
-                    <div class="right justify">
+                    <div class="white-space-8"></div>
+                    <div class="cname">
+                        <h3>Capsicum annuum var. glabriusculum</h3>
+                    </div>
+                    <div class="white-space-32"></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="justify-center">
+            <div class="container row-responsive">
+                <div class="imagen-planta">
+                    <div class="responsive-img" id="planta-img">
+                        <img src="<?php echo SERVER_URL?>application/assets/img/planta2.jpg" alt="Planta">
+                    </div>
+                </div>
+                <div class="informacion-planta">
+                    <div class="column align-center">
                         <div class="container">
                             <div class="column">
-                                <div class="plant-description">
+                                <div class="descripcion-planta">
                                     <h3>DESCRIPCIÓN</h3>
                                 </div>
                                 <div class="white-space-8"></div>
-                                <div class="plant-description">
-                                    <p>Conocidas comúnmente como chiles “chiltepines”, son plantas perennes, herbáceas o tipo arbusto, pequeños, de 0.5 a 2 m de altura, con un solo tallo y muchas ramas ascendente y extendidas; tallos verdes, castillados, pubescentes con pelos incurvados de 0.4 mm de largo o casi lisos. </p>
+                                <div class="descripcion-planta">
+                                    <p>
+                                        Conocidas comúnmente como chiles “chiltepines”, son plantas perennes, herbáceas o tipo arbusto, pequeños, de 0.5 a 2 m de altura, con un solo tallo y muchas ramas ascendente y extendidas; tallos verdes, castillados, pubescentes con pelos incurvados de 0.4 mm de largo o casi lisos.
+                                    </p>
                                 </div>
-
-                                <div class="white-space-16"></div>
-                                <div class="plant-description">
+                                <div class="white-space-24"></div>
+                                <div class="descripcion-planta">
                                     <h3>TAXONOMÍA</h3>
                                 </div>
                                 <div class="white-space-8"></div>
-                                <div class="plant-description">
-                                    <p>Reino: Plantae División: Magnoliophyta Clase: Magnoliopsida Subclase: Asteridae Orden: Solanales Familia: Solanaceae Género: Capsicum Especie: C. annuum var. glabriusculum </p>
+                                <div class="descripcion-planta">
+                                    <p>
+                                        Reino: Plantae División: Magnoliophyta Clase: Magnoliopsida Subclase: Asteridae Orden: Solanales Familia: Solanaceae Género: Capsicum Especie: C. annuum var. glabriusculum
+                                    </p>
                                 </div>
-
-                                <div class="white-space-16"></div>
-                                <div class="plant-description">
+                                <div class="white-space-24"></div>
+                                <div class="descripcion-planta">
                                     <h3>APLICACIONES</h3>
                                 </div>
                                 <div class="white-space-8"></div>
-                                <div class="plant-description">
-                                    <p>Gastronomía Debido a su versatilidad, el chile amashito, es muy utilizado en la cocina tabasqueña, para elaborar salsas y para acompañar sopas y muchos otros platillos típicos del estado. </p>
+                                <div class="descripcion-planta">
+                                    <p>
+                                        Gastronomía Debido a su versatilidad, el chile amashito, es muy utilizado en la cocina tabasqueña, para elaborar salsas y para acompañar sopas y muchos otros platillos típicos del estado.
+                                    </p>
                                 </div>
-
+                                <div class="white-space-32"></div>
                             </div>
                         </div>
+
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-<div class="white-space-25"></div>
